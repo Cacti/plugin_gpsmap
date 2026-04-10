c9724b857a5d18c34e91c207f11e539bf3d30b39
Thomas Vincent <thomasvincent@gmail.com>
2026-04-10 06:57:52 -0700

    fix: remove erroneous backslash-escape from property declarations
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- class/hosts_class.php
+++ class/hosts_class.php
@@ -20,63 +20,63 @@
 */
 
 class host {
-	public string \$id           = '0';
-	public string \$type         = '0';
-	public string \$lat          = '0';
-	public string \$long         = '0';
-	public string \$iprange      = '';
-	public string \$description  = '';
-	public string \$hostname     = '';
-	public string \$radius       = '0';
-	public string \$avail        = '';
-	public string \$status       = '';
-	public int    \$showMap      = 1;
-	public string \$latency      = '0';
-	public int    \$coverage     = 1;
-	public string \$upimage      = '';
-	public string \$downimage    = '';
-	public string \$recoverimage = '';
-	public string \$start        = '0';
-	public string \$stop         = '360';
-	public string \$group        = '0';
+	public string $id           = '0';
+	public string $type         = '0';
+	public string $lat          = '0';
+	public string $long         = '0';
+	public string $iprange      = '';
+	public string $description  = '';
+	public string $hostname     = '';
+	public string $radius       = '0';
+	public string $avail        = '';
+	public string $status       = '';
+	public int    $showMap      = 1;
+	public string $latency      = '0';
+	public int    $coverage     = 1;
+	public string $upimage      = '';
+	public string $downimage    = '';
+	public string $recoverimage = '';
+	public string $start        = '0';
+	public string $stop         = '360';
+	public string $group        = '0';
 
 	public function __construct(
-		string \$id,
-		string \$type,
-		string \$lat,
-		string \$long,
-		string \$iprange,
-		string \$description,
-		string \$hostname,
-		int    \$radius,
-		string \$avail,
-		string \$status,
-		string \$latency,
-		string \$coverage,
-		string \$upimage,
-		string \$downimage,
-		string \$recoverimage,
-		string \$start,
-		string \$stop,
-		string \$group
+		string $id,
+		string $type,
+		string $lat,
+		string $long,
+		string $iprange,
+		string $description,
+		string $hostname,
+		int    $radius,
+		string $avail,
+		string $status,
+		string $latency,
+		string $coverage,
+		string $upimage,
+		string $downimage,
+		string $recoverimage,
+		string $start,
+		string $stop,
+		string $group
 	) {
-		\$this->id           = \$id;
-		\$this->type         = \$type;
-		\$this->lat          = \$lat;
-		\$this->long         = \$long;
-		\$this->iprange      = \$iprange;
-		\$this->description  = \$description;
-		\$this->hostname     = \$hostname;
-		\$this->radius       = (string) \$radius;
-		\$this->avail        = \$avail;
-		\$this->status       = \$status;
-		\$this->latency      = \$latency;
-		\$this->coverage     = (\$coverage === 'on') ? 1 : 0;
-		\$this->upimage      = \$upimage;
-		\$this->downimage    = \$downimage;
-		\$this->recoverimage = \$recoverimage;
-		\$this->start        = \$start;
-		\$this->stop         = \$stop;
-		\$this->group        = \$group;
+		$this->id           = $id;
+		$this->type         = $type;
+		$this->lat          = $lat;
+		$this->long         = $long;
+		$this->iprange      = $iprange;
+		$this->description  = $description;
+		$this->hostname     = $hostname;
+		$this->radius       = (string) $radius;
+		$this->avail        = $avail;
+		$this->status       = $status;
+		$this->latency      = $latency;
+		$this->coverage     = ($coverage === 'on') ? 1 : 0;
+		$this->upimage      = $upimage;
+		$this->downimage    = $downimage;
+		$this->recoverimage = $recoverimage;
+		$this->start        = $start;
+		$this->stop         = $stop;
+		$this->group        = $group;
 	}
 }
